60192f409065e5adfe9afa9120e0b54f3d269796
Amjad Alkahlout <akahloot2005@gmail.com>
2026-01-01 18:45:01 +0530

    modify division function

--- src/functions.java
+++ src/functions.java
@@ -9,8 +9,13 @@ public class Functions {
         System.out.println("The result of " +num1+"*"+num2+"="+ result);
     }
     public void division(int num1,int num2){
-        int result =num1/num2;
-        System.out.println("The result of " +num1+"/"+num2+"="+ result);
+        try {
+            int result = num1 / num2;
+            System.out.println("The result of " + num1 + "/" + num2 + "=" + result);
+        }
+        catch (ArithmeticException e){
+            System.out.println("can not divide a number by zero");
+        }
     }
     public void subtraction(int num1,int num2){
         int result =num1-num2;
