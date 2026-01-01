fd0db6fd0292c276e41fea1d0159571dcd19dc98
Amjad Alkahlout <akahloot2005@gmail.com>
2026-01-01 19:17:58 +0530

    final test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,42 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        Functions f =new Functions();
+        int num1,num2;
+        int choice;
+        char ch ='Y';
+        while (ch =='Y') {
+            System.out.println("\t\t\tBasic Calculator\t\t\t");
+            System.out.println("1- Addition\n2- Subtraction\n3- Multiplication\n4- Division\n5- Exit");
+            System.out.print("select the operation: ");
+            choice = in.nextInt();
+            if (choice==5){
+                System.exit(0);
+            }
+                System.out.println("Enter the operands: ");
+                num1 = in.nextInt();
+                num2 = in.nextInt();
 
-        System.out.println("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+            switch (choice) {
+                case 1:f.addition(num1,num2);
+                    break;
+                case 2:f.subtraction(num1,num2);
+                    break;
+                case 3:f.multiply(num1,num2);
+                    break;
+                case 4:f.division(num1,num2);
+                    break;
+                default:
+                    System.out.println("invalid choice");
+            }
+            System.out.println("Do you want to continue Y/N :");
+             ch = in.next().charAt(0);
+        }
+
+
+
         }
     }
-}
+
--- src/functions.java
+++ src/functions.java
@@ -1,17 +1,17 @@
 public class Functions {
     public void addition(int num1,int num2){
         int result = num1+num2;
-        System.out.println("The result of " +num1+"+"+num2+"="+ result);
+        System.out.println("The result of " +num1+"+"+num2+"= "+ result);
     }
 
     public void multiply(int num1,int num2){
         int result = num1*num2;
-        System.out.println("The result of " +num1+"*"+num2+"="+ result);
+        System.out.println("The result of " +num1+"*"+num2+"= "+ result);
     }
     public void division(int num1,int num2){
         try {
             int result = num1 / num2;
-            System.out.println("The result of " + num1 + "/" + num2 + "=" + result);
+            System.out.println("The result of " + num1 + "/" + num2 + "= " + result);
         }
         catch (ArithmeticException e){
             System.out.println("can not divide a number by zero");
@@ -19,6 +19,6 @@ public class Functions {
     }
     public void subtraction(int num1,int num2){
         int result =num1-num2;
-        System.out.println("The result of " +num1+"-git"+num2+"="+ result);
+        System.out.println("The result of " +num1+"-"+num2+"= "+ result);
     }
 }
