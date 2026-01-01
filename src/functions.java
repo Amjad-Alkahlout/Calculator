bf101fc06d5b9151c557a5871691f7fdc82f0fec
Amjad Alkahlout <akahloot2005@gmail.com>
2026-01-01 18:06:27 +0530

    add multiplication ,division and subtraction

--- src/functions.java
+++ src/functions.java
@@ -1,6 +1,19 @@
 public class functions {
-    public void addNumber(int x,int y){
-        int result = x+y;
-        System.out.println("the sum of "+x +" and "+y+" =" +result);
+    public void addition(int num1,int num2){
+        int result = num1+num2;
+        System.out.println("The result of " +num1+"+"+num2+"="+ result);
+    }
+
+    public void multiply(int num1,int num2){
+        int result = num1*num2;
+        System.out.println("The result of " +num1+"*"+num2+"="+ result);
+    }
+    public void division(int num1,int num2){
+        int result =num1/num2;
+        System.out.println("The result of " +num1+"/"+num2+"="+ result);
+    }
+    public void subtraction(int num1,int num2){
+        int result =num1-num2;
+        System.out.println("The result of " +num1+"-git"+num2+"="+ result);
     }
 }
